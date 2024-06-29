7b9c626ee6321dc5f82472e00cde8645c23e3d5c
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2024-06-29 22:07:25 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -7,12 +7,18 @@ contract Assessment {
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
     event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+    event FundsTransferred(address indexed to, uint256 amount);
 
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-    function getBalance() public view returns(uint256){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
@@ -23,8 +29,7 @@ contract Assessment {
 
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
         if (address(this).balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: address(this).balance,
@@ -42,10 +47,26 @@ contract Assessment {
         return owner;
     }
 
-    function changeOwner(address payable newOwner) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function changeOwner(address payable newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner address cannot be zero");
         address oldOwner = owner;
         owner = newOwner;
         emit OwnerChanged(oldOwner, newOwner);
+    }
+
+    function transferFunds(address payable to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Cannot transfer to zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        if (address(this).balance < amount) {
+            revert InsufficientBalance({
+                balance: address(this).balance,
+                withdrawAmount: amount
+            });
+        }
+
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "Transfer failed");
+
+        emit FundsTransferred(to, amount);
     }
 }
